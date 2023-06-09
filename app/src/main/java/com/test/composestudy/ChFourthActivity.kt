7558fd90c1c502fccd29c8763533b81e2ad4ff5f
SsongSik <sgsk88@nate.com>
2023-06-10 01:03:48 +0900

    [Compose] Canvas

--- app/src/main/java/com/test/composestudy/ChFourthActivity.kt
+++ app/src/main/java/com/test/composestudy/ChFourthActivity.kt
@@ -3,14 +3,19 @@ package com.test.composestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
@@ -29,9 +34,24 @@ class ChFourthActivity : ComponentActivity() {
 //                ConstraintLayoutEx()
 //                ConstraintSetEx()
 //                ConstraintLayoutEx2()
-                CardEx(cardData)
+//                CardEx(cardData)
+                CanvasEx()
             }
         }
+    }
+}
+
+@Composable
+fun CanvasEx() {
+    Canvas(modifier = Modifier.size(20.dp)) {
+        drawLine(Color.Red, Offset(30f, 10f), Offset(50f, 40f))
+
+        drawCircle(Color.Yellow, 10f, Offset(25f, 40f))
+        drawRect(Color.Magenta, Offset(25f, 40f), Size(10f, 10f))
+
+//        Icons.Filled.Send
+        drawLine(Color.Green, Offset(2.01f, 21.0f), Offset(23.0f, 12.0f))
+        drawLine(Color.Green, Offset(23.0f, 12.0f), Offset(2.01f, 3.0f))
     }
 }
 
@@ -286,6 +306,7 @@ fun DefaultPreviewCh4() {
 //        ConstraintLayoutEx()
 //        ConstraintSetEx()
 //        ConstraintLayoutEx2()
-        CardEx2(ProfileCardActivity.cardData)
+//        CardEx2(ProfileCardActivity.cardData)
+        CanvasEx()
     }
 }
