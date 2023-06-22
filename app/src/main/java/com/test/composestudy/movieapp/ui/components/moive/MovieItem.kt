dbeb1a814c02ba68b53f1c1de1d13f6bcf402994
SsongSik <sgsk88@nate.com>
2023-06-23 00:17:25 +0900

    [Compose] Movie App Category Add

--- app/src/main/java/com/test/composestudy/movieapp/ui/components/moive/MovieItem.kt
+++ app/src/main/java/com/test/composestudy/movieapp/ui/components/moive/MovieItem.kt
@@ -3,11 +3,14 @@ package com.test.composestudy.movieapp.ui.components.moive
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,6 +39,16 @@ fun MovieItem() {
             modifier = Modifier.padding(vertical = 10.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(12.dp),
+                imageVector = ImageVector.vectorResource(id = com.test.composestudy.R.drawable.baseline_star_rate_24),
+//                tint = Color.Black.copy(
+//                    alpha = 0.5f
+//                ),
+                contentDescription = "rating icon"
+            )
             Text(
                 text = "4.5"
             )
